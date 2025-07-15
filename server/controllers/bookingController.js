f0cece545a2c758a2718092d43cf15faab442347
Sushan999 <thapasusan777@gmail.com>
2025-07-15 21:23:39 +0545

    Add booking apis

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,3 +1,125 @@
-//Function to check availability of car
+import Booking from "../models/Booking.js";
+import Car from "../models/Car.js";
 
-const checkAvailability = async (car, pickupDate, returnDate) => {};
+//Function to check availability of car for givern date
+export const checkAvailability = async (car, pickupDate, returnDate) => {
+  const bookings = await Booking.find({
+    car,
+    pickupDate: { lte: returnDate },
+    pickupDate: { lte: pickupDate },
+  });
+  return bookings.length === 0;
+};
+
+//API to check availability of Cars for the given date and location
+export const checkAvailabilityOfCar = async (req, res) => {
+  try {
+    const { location, pickupDate, returnDate } = req.body;
+    //fetch all available cars in given location
+    const cars = await CaretPosition.find({ location, isAvailable: true });
+
+    //check car availability for the given date range using promise
+    const availableCarsPromises = cars.map(async (car) => {
+      const isAvailable = await checkAvailability(
+        car._id,
+        pickupDate,
+        returnDate
+      );
+      return { ...car._doc, isAvailable: isAvailable };
+    });
+
+    let availableCars = await Promise.all(availableCarsPromises);
+    availableCars = availableCars.filter((car) => car.isAvailable === true);
+
+    res.json({ sucess: true, availableCars });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ sucess: false, message: error.message });
+  }
+};
+
+//API to create Booking
+export const createBooking = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const { car, pickupDate, returnDate } = req.body;
+
+    const isAvailable = await checkAvailability(car, pickupDate, returnDate);
+    if (!isAvailable) {
+      return res.json({ sucess: false, message: "Car is not available" });
+    }
+
+    const carData = await Car.findById(car);
+    //calculate price based on pickupdate and returndate
+    const picked = new Date(pickupDate);
+    const returned = new Date(returnDate);
+    const noOfDays = Math.ceil((returned - picked) / (1000 * 60 * 60 * 24));
+    const price = carData.pricePerDay * noOfDays;
+
+    await Booking.create({
+      car,
+      owner: carData.owner,
+      user: _id,
+      pickupDate,
+      returnDate,
+      price,
+    });
+    req.json({ sucess: true, message: "Booking Created" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ sucess: false, message: error.message });
+  }
+};
+
+//API to list user Bookings
+export const getUserBookings = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const bookings = await Booking.find({ user: _id })
+      .populate("car")
+      .sort({ createdAt: -1 });
+    res.json({ sucess: true, bookings });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ sucess: false, message: error.message });
+  }
+};
+
+//Api to get owner Bookings
+export const getOwnerBookings = async (req, res) => {
+  try {
+    if (req.user.role !== "owner") {
+      return res.json({ sucess: false, message: "Unauthorized" });
+    }
+    const bookings = await Booking.find({ owner: req.User._id })
+      .populate("car user")
+      .select("-user.password")
+      .sort({ createdAt: -1 });
+    res.json({ sucess: true, bookings });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ sucess: false, message: error.message });
+  }
+};
+
+//Update booking status
+export const changeBookingStatus = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const { bookingId, status } = req.body;
+
+    const booking = await Booking.findById(bookingId);
+
+    if (booking.owner.toString() !== _id.toString()) {
+      return res.json({ sucess: false, message: "Unauthorized" });
+    }
+
+    booking.staus = status;
+    await booking.save();
+
+    res.json({ sucess: true, message: "Status Updated" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ sucess: false, message: error.message });
+  }
+};
---
+++ server/routes/bookingRoutes.js
@@ -0,0 +1,6 @@
+import express from "express";
+import { checkAvailabilityOfCar } from "../controllers/bookingController.js";
+
+const bookingRouter = express.Router();
+
+bookingRouter.post("check-availability", checkAvailabilityOfCar);
